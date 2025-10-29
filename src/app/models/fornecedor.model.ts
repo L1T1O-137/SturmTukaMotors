4409bac5576b75f985e9622395d243d8ff1d3bec
Erick-137 <basillisky@gmail.com>
2025-10-29 19:45:17 -0300

    Consertando mais error 29/10-19:45

--- src/app/models/fornecedor.model.ts
+++ src/app/models/fornecedor.model.ts
@@ -1,2 +1,6 @@
 export interface Fornecedor {
-}
+    id?: number;
+    nome: string;
+    cnpj: string;
+    fone: string;
+    }
--- src/app/services/db.service.ts
+++ src/app/services/db.service.ts
@@ -1,20 +1,14 @@
 import { Injectable } from '@angular/core';
 import Dexie, { Table } from 'dexie';
-import { Fornecedor } from '../models/fornecedor.model'; 
-
-
-@Injectable({
-  providedIn: 'root'
-})
+import { Fornecedor } from '../models/fornecedor.model';
+@Injectable({ providedIn: 'root' })
 export class DbService extends Dexie {
   fornecedores!: Table<Fornecedor, number>;
   constructor() {
-    super('SturmTukaDB');
+    super('LavaCarDB');
     this.version(1).stores({
       fornecedores: '++id, nome, cnpj, fone',
     });
   }
-
-
 }
-  export const db = new DbService();
+export const db = new DbService();
